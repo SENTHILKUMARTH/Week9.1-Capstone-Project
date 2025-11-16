28b1bef7f10d02f348f3f6be9c9016eac1434c2f
SENTHILKUMARTH <thiru.h.senthilkumar@gmail.com>
2025-11-16 22:57:58 +0530

    Add files via upload

---
+++ Week9.1-Capstone Project/2.Data Preprocessing/StudentPlacementMetadata.py
@@ -0,0 +1,14 @@
+class StudentPlacementMetadata():
+
+    def quanQual(dataset):
+        quan=[]
+        qual=[]
+        for columnName in dataset.columns:
+            #print(columnName)
+            if(dataset[columnName].dtype=='O'):
+                #print("qual")
+                qual.append(columnName)
+            else:
+                #print("quan")
+                quan.append(columnName)
+        return quan,qual
